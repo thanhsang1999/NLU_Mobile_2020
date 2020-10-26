fce8acdcfd0640f90a21c6a79b391e37a435cc66
hearterzouest <hearterzouest99.999@gamil.com>
2020-10-26 19:58:04 +0700

    Fix bug

--- app/src/main/java/com/example/mobile/WellComeActivity.java
+++ app/src/main/java/com/example/mobile/WellComeActivity.java
@@ -9,17 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
-import com.synnapps.carouselview.ViewListener;
 
 public class WellComeActivity extends AppCompatActivity {
     int[] image= new int[]{R.drawable.ic_wellcome_1, R.drawable.ic_wellcome_2,R.drawable.ic_wellcome_3};
 //    String [] titles={getString(R.string.wellcome),getString(R.string.wellcome_1),getString(R.string.wellcome_2),getString(R.string.wellcome_3)};
-    int [] titles2={R.string.wellcome_1,R.string.wellcome_2,R.string.wellcome_3};
+    int [] titleImages ={R.string.wellcome_1,R.string.wellcome_2,R.string.wellcome_3};
     TextView textViewDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +30,23 @@ public class WellComeActivity extends AppCompatActivity {
             public void setImageForPosition(int position, ImageView imageView) {
                 imageView.setImageResource(image[position]);
 
+
             }
         });
         carouselView.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                textViewDescription.setText(titles2[position]);
+                textViewDescription.setText(titleImages[position]);
             }
 
             @Override
             public void onPageSelected(int position) {
-                textViewDescription.setText(titles2[position]);
+
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
+
 
             }
         });
