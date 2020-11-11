f0316871b50e0b4b74ca7d5d877dd29046d890d8
hearterzouest <hearterzouest99.999@gamil.com>
2020-11-11 13:46:48 +0700

    update ui sign up activity

--- app/src/main/java/com/example/mobile/SignUpActivity.java
+++ app/src/main/java/com/example/mobile/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        TextView textViewSignIn=findViewById(R.id.textViewSignIn);
+        textViewSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
