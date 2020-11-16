7f1d7d2a043bc68b2c25cb0a1a8e73505c1222a8
thiendaopk1 <thiendaopk1@gmail.com>
2020-11-16 17:47:32 +0700

    finish arrowback

--- app/src/main/java/com/example/mobile/LogInActivity.java
+++ app/src/main/java/com/example/mobile/LogInActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,15 +28,18 @@ public class LogInActivity extends AppCompatActivity {
     EditText editTextUserName;
     EditText editTextPassword;
     private ProgressBar spinner;
+    ImageView backArrow;
+    Button btn_log_in;
+    TextView textViewForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        editTextUserName=findViewById(R.id.editTextUserName);
-        editTextPassword=findViewById(R.id.editTextPassword);
-        spinner = (ProgressBar)findViewById(R.id.progressBar);
+        as();
+        backArrow=findViewById(R.id.backArrow);
+
         spinner.setVisibility(View.GONE);
         TextView textViewSignUp=findViewById(R.id.textViewSignUp);
         textViewSignUp.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +49,7 @@ public class LogInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        TextView textViewForgot=findViewById(R.id.textViewForgot);
+
         textViewForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,8 +57,14 @@ public class LogInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         connectionWebService = new ConnectionWebService(this);
-        Button btn_log_in=findViewById(R.id.btn_log_in);
+
         btn_log_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,6 +99,14 @@ public class LogInActivity extends AppCompatActivity {
 
 
     }
+    public void as(){
+        editTextUserName=findViewById(R.id.editTextUserName);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+        textViewForgot=findViewById(R.id.textViewForgot);
+        btn_log_in=findViewById(R.id.btn_log_in);
+    }
+
     public void loading(View view){
         spinner.setVisibility(View.VISIBLE);
     }
