3251d801a21896e93caab280bc1afece4b466d28
hearterzouest <hearterzouest99.999@gamil.com>
2020-11-18 18:42:53 +0700

    first create ui

--- app/src/main/java/com/example/mobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mobile/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,13 +23,14 @@ public class HomeFragment extends Fragment {
         homeViewModel = new
                 ViewModelProvider(this.requireActivity()).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final GridLayout gridLayout = root.findViewById(R.id.grid_layout);
+
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
