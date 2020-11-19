4ea13024710f8383da605ab201bc4df05b30ae21
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2020-11-19 15:50:55 +0700

    new note 1.0

--- app/src/main/java/com/example/mobile/NewNoteActivity.java
+++ app/src/main/java/com/example/mobile/NewNoteActivity.java
@@ -2,16 +2,24 @@ package com.example.mobile;
 
 
 import android.app.ActionBar;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class NewNoteActivity extends AppCompatActivity {
     EditText editTextTitle,editTextContent;
+    ConstraintLayout Mainlayout,contentLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +33,33 @@ public class NewNoteActivity extends AppCompatActivity {
         init();
         editTextContent.requestFocus();
 
+
     }
 
     private void init() {
         editTextTitle = findViewById(R.id.editTextTitle);
         editTextContent = findViewById(R.id.editTextContent);
+        Mainlayout = findViewById(R.id.mainLayout);
+        contentLayout = findViewById(R.id.contentLayout);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==android.R.id.home){
-            finish();
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.menuChangeColor:
+                contentLayout.setBackgroundResource(R.drawable.ic_bgblue1);
+                return true;
+            default:return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.new_note_menu,menu);
+        return true;
     }
 }
