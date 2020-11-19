f18334d28670010468a61936dca6e9a83df16f6c
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2020-11-19 16:11:22 +0700

    Revert "change to gridview"
    This reverts commit 77786abb62e51422d9560fb6b03bcf44adda3921.

--- app/src/main/java/com/example/mobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mobile/ui/home/HomeFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
-
-import com.example.mobile.HomeItemAdapter;
-import com.example.mobile.Note;
 import com.example.mobile.R;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -31,13 +23,7 @@ public class HomeFragment extends Fragment {
         homeViewModel = new
                 ViewModelProvider(this.requireActivity()).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final GridView gridView = root.findViewById(R.id.gridView);
-        List<Note> notes= new ArrayList<>();
-        notes.add(new Note(1,"One", "....................", new Date(), new Date()));
-        notes.add(new Note(2,"Two", "....................", new Date(), new Date()));
-
-
-        gridView.setAdapter(new HomeItemAdapter(this.getActivity(), notes));
+        final GridLayout gridLayout = root.findViewById(R.id.grid_layout);
 
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
