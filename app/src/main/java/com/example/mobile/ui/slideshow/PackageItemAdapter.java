b698868d7725eebd94446c9bf253814625458e4e
hearterzouest <hearterzouest99.999@gamil.com>
2020-11-28 17:12:42 +0700

    delete fast blur

--- app/src/main/java/com/example/mobile/ui/slideshow/PackageItemAdapter.java
+++ app/src/main/java/com/example/mobile/ui/slideshow/PackageItemAdapter.java
@@ -41,7 +41,7 @@ import java.util.List;
 
 public class PackageItemAdapter extends BaseAdapter {
 
-    private List<Package> _items;
+    private final List<Package> _items;
     Activity activity;
     Fragment fragment;
     ConnectionDatabaseLocalMobile c;
@@ -73,7 +73,7 @@ public class PackageItemAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         // 1
         final Package book = _items.get(position);
-        ViewHolder viewHolder = null;
+        ViewHolder viewHolder;
 
         // 2
         if (convertView == null) {
@@ -89,7 +89,7 @@ public class PackageItemAdapter extends BaseAdapter {
 
 
 
-            Drawable top = this.activity.getResources().getDrawable(R.drawable.ic_add_note);
+        Drawable top = this.activity.getResources().getDrawable(R.drawable.ic_add_note);
 
 
 
@@ -100,33 +100,17 @@ public class PackageItemAdapter extends BaseAdapter {
                 viewHolder.textView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-//                        MyModal myModal = new MyModal();
-//                        CreatePackageDialogFragment myModal = new CreatePackageDialogFragment();
-//                        final Dialog dialog = new Dialog(fragment.getActivity(), R.style.Theme);
-//                        final Dialog dialog = new Dialog(fragment.getActivity());
                         final Dialog dialog = new Dialog(fragment.getActivity(), R.style.CustomDialogTheme);
                         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                         dialog.setContentView(R.layout.create_packed);
-
-
-
-//                        View beneathView = fragment.getActivity().getWindow().getDecorView();
-//
-//
-//                                BlurDrawable blurDrawable = new BlurDrawable(beneathView, 100);
-//
-//                        dialog.getWindow().setBackgroundDrawable(blurDrawable);
-
-
-                        Bitmap takeScreenShot=takeScreenShot(fragment.getActivity());
-
-
-                        Bitmap fast= fastblur(takeScreenShot,5);
-//                        Bitmap fast= blur(takeScreenShot,10000000);
-
-                        final Drawable draw=new BitmapDrawable(fragment.getActivity().getResources(),fast);
-                        dialog.getWindow().setBackgroundDrawable(draw);
                         dialog.show();
+                        new Thread(()->{
+                            Bitmap takeScreenShot=takeScreenShot(fragment.getActivity());
+
+                            Bitmap fast= blur(takeScreenShot,10000000);
+                            final Drawable draw=new BitmapDrawable(fragment.getActivity().getResources(),fast);
+                            dialog.getWindow().setBackgroundDrawable(draw);
+                        }).run();
 
 
 
@@ -359,167 +343,6 @@ public class PackageItemAdapter extends BaseAdapter {
         Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height - statusBarHeight);
         view.destroyDrawingCache();
         return b;
-    }
-    public Bitmap fastblur(Bitmap sentBitmap, int radius) {
-        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-        if (radius < 1) {
-            return (null);
-        }
-        int w = bitmap.getWidth();
-        int h = bitmap.getHeight();
-        int[] pix = new int[w * h];
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-        int wm = w - 1;
-        int hm = h - 1;
-        int wh = w * h;
-        int div = radius + radius + 1;
-        int r[] = new int[wh];
-        int g[] = new int[wh];
-        int b[] = new int[wh];
-        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-        int vmin[] = new int[Math.max(w, h)];
-        int divsum = (div + 1) >> 1;
-        divsum *= divsum;
-        int dv[] = new int[256 * divsum];
-        for (i = 0; i < 256 * divsum; i++) {
-            dv[i] = (i / divsum);
-        }
-        yw = yi = 0;
-        int[][] stack = new int[div][3];
-        int stackpointer;
-        int stackstart;
-        int[] sir;
-        int rbs;
-        int r1 = radius + 1;
-        int routsum, goutsum, boutsum;
-        int rinsum, ginsum, binsum;
-        for (y = 0; y < h; y++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            for (i = -radius; i <= radius; i++) {
-                p = pix[yi + Math.min(wm, Math.max(i, 0))];
-                sir = stack[i + radius];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-                rbs = r1 - Math.abs(i);
-                rsum += sir[0] * rbs;
-                gsum += sir[1] * rbs;
-                bsum += sir[2] * rbs;
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-            }
-            stackpointer = radius;
-            for (x = 0; x < w; x++) {
-                r[yi] = dv[rsum];
-                g[yi] = dv[gsum];
-                b[yi] = dv[bsum];
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-                if (y == 0) {
-                    vmin[x] = Math.min(x + radius + 1, wm);
-                }
-                p = pix[yw + vmin[x]];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[(stackpointer) % div];
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-                yi++;
-            }
-            yw += w;
-        }
-        for (x = 0; x < w; x++) {
-            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-            yp = -radius * w;
-            for (i = -radius; i <= radius; i++) {
-                yi = Math.max(0, yp) + x;
-                sir = stack[i + radius];
-                sir[0] = r[yi];
-                sir[1] = g[yi];
-                sir[2] = b[yi];
-                rbs = r1 - Math.abs(i);
-                rsum += r[yi] * rbs;
-                gsum += g[yi] * rbs;
-                bsum += b[yi] * rbs;
-                if (i > 0) {
-                    rinsum += sir[0];
-                    ginsum += sir[1];
-                    binsum += sir[2];
-                } else {
-                    routsum += sir[0];
-                    goutsum += sir[1];
-                    boutsum += sir[2];
-                }
-                if (i < hm) {
-                    yp += w;
-                }
-            }
-            yi = x;
-            stackpointer = radius;
-            for (y = 0; y < h; y++) {
-                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
-                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
-                rsum -= routsum;
-                gsum -= goutsum;
-                bsum -= boutsum;
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
-                routsum -= sir[0];
-                goutsum -= sir[1];
-                boutsum -= sir[2];
-                if (x == 0) {
-                    vmin[y] = Math.min(y + r1, hm) * w;
-                }
-                p = x + vmin[y];
-                sir[0] = r[p];
-                sir[1] = g[p];
-                sir[2] = b[p];
-                rinsum += sir[0];
-                ginsum += sir[1];
-                binsum += sir[2];
-                rsum += rinsum;
-                gsum += ginsum;
-                bsum += binsum;
-                stackpointer = (stackpointer + 1) % div;
-                sir = stack[stackpointer];
-                routsum += sir[0];
-                goutsum += sir[1];
-                boutsum += sir[2];
-                rinsum -= sir[0];
-                ginsum -= sir[1];
-                binsum -= sir[2];
-                yi += w;
-            }
-        }
-        Log.e("pix", w + " " + h + " " + pix.length);
-        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-        return (bitmap);
     }
 
 }
