7354df21b19ca43bd4c4bd7d4f97c7990943ddb6
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2021-01-15 05:27:26 +0700

    fix local network sang

--- app/src/main/java/com/example/mobile/Config.java
+++ app/src/main/java/com/example/mobile/Config.java
@@ -4,12 +4,13 @@ import android.util.Log;
 
 public class Config {
     public static boolean IS_LOCAL = false;
-    public static String URL_LOCAL_SANG = "http://192.168.0.222/mobile/";
+    public static String URL_LOCAL_SANG = "http://192.168.0.222:8080/mobile/";
+    public static String URL_LOCAL_SANG2 = "http://192.168.137.1:8080/mobile";
     public static String URL_LOCAL = "http://192.168.0.111/mobile/";
     public static String URL_GLOBAL = "https://mobilenlu2020.000webhostapp.com/";
     public static String getURL(){
-        Log.e("DATABASE",  (IS_LOCAL)? URL_LOCAL:URL_GLOBAL);
-        return (IS_LOCAL)? URL_LOCAL:URL_GLOBAL;
+        Log.e("DATABASE",  (IS_LOCAL)? URL_LOCAL_SANG2:URL_GLOBAL);
+        return (IS_LOCAL)? URL_LOCAL_SANG:URL_GLOBAL;
     };
 
 }
