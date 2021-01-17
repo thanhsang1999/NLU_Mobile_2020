efea5f78bca97804ba46b332c449210a6e9ec22f
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2021-01-18 01:33:09 +0700

    fix giao dien newnote

--- app/src/main/java/com/example/mobile/Config.java
+++ app/src/main/java/com/example/mobile/Config.java
@@ -3,10 +3,10 @@ package com.example.mobile;
 import android.util.Log;
 
 public class Config {
-    public static boolean IS_LOCAL = false;
+    public static boolean IS_LOCAL = true;
 
-//    public static String URL_LOCAL = "http://192.168.0.222:8080/mobile/";//SANG
-    public static String URL_LOCAL = "http://192.168.0.111/mobile/";//HOANG
+    public static String URL_LOCAL = "http://192.168.0.222:8080/mobile/";//SANG
+//    public static String URL_LOCAL = "http://192.168.0.111/mobile/";//HOANG
     public static String URL_GLOBAL = "https://mobilenlu2020.000webhostapp.com/";//INTERNET
     public static String getURL(){
         Log.e("DATABASE",  (IS_LOCAL)? URL_LOCAL:URL_GLOBAL);
